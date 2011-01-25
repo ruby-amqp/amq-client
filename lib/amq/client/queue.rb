7d75f95a8c713bbab2e46993a8ff05e123401f66
Jakub Šťastný aka Botanicus <stastny@101ideas.cz>
2011-01-25 12:04:15 +0000

    The Basic class doesn't really match the OOP schema, let's move it.

--- lib/amq/client/queue.rb
+++ lib/amq/client/queue.rb
@@ -23,10 +23,22 @@ module AMQ
       end
 
       # Basic.Consume
-      # This is a bit tricky, how to route the message to responding Queue instance? Well, we know the exchange, so we can do exchange.queues.each ...
-      # ???? Is this really un-opinionated? AMQP specifies it as BASIC.Consume, not Queue.Consume and this is supposed to be no more than just AMQP assembler for Ruby. On the other hand it's linked with the queue object.
       def consume(&block)
+        if @consumer_tag
+          raise RuntimeError.new("This instance is already consumed! Create another one using #dup.")
+        end
+        @consumer_tag = "random sh1t3"
+        adapter.client.consumers[@consumer_tag] = self ### WHAT IF there'll be more consume blocks for the same object? Now that's about opinion, but we are NOT building an opinionated API here!!!!
         self.callbacks[:consume] = block
+      end
+
+      def dup
+        if @name.eql?("")
+          raise RuntimeError.new("You can't clone anonymous queue until it receives back the name in Queue.Declare-Ok response. Move the code with #dup to the callback for the #declare method.")
+        end
+        instance = self.dup
+        instance.instance_variable_set(:@consumer_tag, nil)
+        instance
       end
     end
 
@@ -39,6 +51,13 @@ module AMQ
 
     self.handle(Protocol::Queue::BindOk) do |client, frame|
 
+    end
+
+    # Basic.Deliver
+    self.handle(Protocol::Basic::Deliver) do |client, method, header, *body|
+      queue = client.consumers[method.consumer_tag]
+      body  = body.reduce("") { |buffer, frame| buffer += frame.body }
+      queue.exec_callback(:consume, body)
     end
   end
 end
