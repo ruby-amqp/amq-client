4d5817650c2a51a4ddc6dce1509bcfd1684f8ab3
Jakub Šťastný aka Botanicus <stastny@101ideas.cz>
2011-02-04 13:37:09 +0000

    Refactoring, specs.

---
+++ lib/amq/client/entity.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+module AMQ
+  class Client
+    class Entity
+      attr_reader :callbacks
+      def initialize(adapter)
+        @adapter, @callbacks = adapter, Hash.new
+      end
+
+      def exec_callback(name, *args, &block)
+        callback = self.callbacks[name]
+        callback.call(self, *args, &block)
+      end
+    end
+  end
+end
---
+++ spec/client/entity_spec.rb
@@ -0,0 +1,52 @@
+# encoding: utf-8
+
+require "ostruct"
+require "spec_helper"
+require "amq/client/entity"
+
+describe AMQ::Client::Entity do
+  subject do
+    AMQ::Client::Entity.new(OpenStruct.new(name: "adapter"))
+  end
+
+  it "should maintain an associative array of callbacks" do
+    subject.callbacks.should be_kind_of(Hash)
+  end
+
+  describe "#exec_callback" do
+    it "should call given callback" do
+      subject.callbacks[:init] = begin
+        Proc.new do |*args, &block|
+          @called = true
+        end
+      end
+
+      subject.exec_callback(:init)
+      @called.should be_true
+    end
+
+    it "should pass itself as the first argument" do
+      subject.callbacks[:init] = begin
+        Proc.new { |entity, *args| entity }
+      end
+
+      subject.exec_callback(:init).should eql(subject)
+    end
+
+    it "should pass arguments to the callback" do
+      subject.callbacks[:init] = begin
+        Proc.new { |entity, *args| args }
+      end
+
+      subject.exec_callback(:init, 1).should eql([1])
+    end
+
+    it "should pass block to the callback" do
+      subject.callbacks[:init] = begin
+        Proc.new { |*args, &block| block.call }
+      end
+
+      subject.exec_callback(:init) { "block" }.should eql("block")
+    end
+  end
+end
