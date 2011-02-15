9d5c91aeb60cf0ee4398ae18f4f332bd5a85095d
Jakub Šťastný aka Botanicus <stastny@101ideas.cz>
2011-02-15 18:55:21 +0000

    Bundle install for all rubies.

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -2,13 +2,14 @@
 
 echo -e "\n\n==== Setup ===="
 git fetch && git reset origin/master --hard
-bundle install --local
 
 echo -e "\n\n==== Ruby 1.9.2 Head ===="
+rvm 1.9.2-head exec bundle install --local; echo
 rvm 1.9.2-head exec rspec spec
 return_status=$?
 
 echo -e "\n\n==== Ruby 1.8.7 ===="
+rvm 1.8.7 exec bundle install --local; echo
 rvm 1.8.7 exec rspec spec
 return_status=$(expr $return_status + $?)
 
