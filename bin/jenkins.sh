2ff669143df3381c8a41bd78f6feb3368ffa6366
Jakub Šťastný aka Botanicus <stastny@101ideas.cz>
2011-02-15 18:48:50 +0000

    Return proper status.

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -6,6 +6,10 @@ bundle install --local
 
 echo -e "\n\n==== Ruby 1.9.2 Head ===="
 rvm 1.9.2-head exec rspec spec
+return_status=$?
 
 echo -e "\n\n==== Ruby 1.8.7 ===="
 rvm 1.8.7 exec rspec spec
+return_status=$($return_status + $?)
+
+test $return_status -eq 0
