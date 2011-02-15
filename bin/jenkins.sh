07f569c89502bbb0105fd365f7ece93218e060e9
Jakub Šťastný aka Botanicus <stastny@101ideas.cz>
2011-02-15 18:39:44 +0000

    Some formatting, run both 1.9.2 & 1.8.7.

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -3,5 +3,12 @@
 # Server-specific, I have RVM setup at the profile file:
 source /etc/profile
 
+echo "<h3>Setup</h3>"
 git fetch && git reset origin/master --hard
-rvm 1.9.2-head,1.8.7 exec bundle install --local && rspec spec
+bundle install --local
+
+echo "<h3>Ruby 1.9.2 Head</h3>"
+rvm 1.9.2-head exec rspec spec
+
+echo "<h3>Ruby 1.8.7</h3>"
+rvm 1.8.7 exec rspec spec
