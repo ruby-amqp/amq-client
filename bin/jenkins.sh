2a8ebe801720b06e0904d4158591fb0e2ea3a0e7
Jakub Šťastný aka Botanicus <stastny@101ideas.cz>
2011-02-15 18:42:44 +0000

    Ah, Linux, we need echo -e.

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "==== Setup ===="
+echo -e "\n\n==== Setup ===="
 git fetch && git reset origin/master --hard
 bundle install --local
 
-echo "\n\n==== Ruby 1.9.2 Head ===="
+echo -e "\n\n==== Ruby 1.9.2 Head ===="
 rvm 1.9.2-head exec rspec spec
 
-echo "\n\n==== Ruby 1.8.7 ===="
+echo -e "\n\n==== Ruby 1.8.7 ===="
 rvm 1.8.7 exec rspec spec
