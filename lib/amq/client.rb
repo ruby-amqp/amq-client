434640d3df33d82174f838804fb877d7fb185b48
Michael S. Klishin <michael@novemberain.com>
2011-02-18 19:45:24 +0300

    This is amq-client. There is no need to load server-specific framing.

--- lib/amq/client.rb
+++ lib/amq/client.rb
@@ -14,7 +14,7 @@ require "amq/client/exceptions"
 require "amq/client/adapter"
 
 begin
-  require "amq/protocol/client" # TODO: what about server?
+  require "amq/protocol/client"
 rescue LoadError => exception
   if exception.message.match("amq/protocol")
     raise LoadError.new("You have to install amq-protocol library first!")
