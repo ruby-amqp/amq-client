30676127573da60828e49f814c349a6f6df51635
Michael S. Klishin <michael@novemberain.com>
2011-02-19 15:33:53 +0300

    Document default adapter settings

--- lib/amq/client/settings.rb
+++ lib/amq/client/settings.rb
@@ -4,7 +4,11 @@ require "amq/protocol/client" # TODO: "amq/protocol/constants"
 
 module AMQ
   module Client
+    # @see AMQ::Client::Settings.configure
     module Settings
+      # Default connection settings used by AMQ clients
+      #
+      # @see AMQ::Client::Settings.configure
       def self.default
         {
           # server
@@ -31,14 +35,30 @@ module AMQ
         }
       end
 
+      # Merges given configuration parameters with defaults and returns
+      # the result.
+      #
+      # @param [Hash] Configuration parameters to use.
+      #
+      # @option settings [String] :host ("127.0.0.1") Hostname AMQ broker runs on.
+      # @option settings [String] :port (5672) Port AMQ broker listens on.
+      # @option settings [String] :vhost ("/") Virtual host to use.
+      # @option settings [String] :user ("guest") Username to use for authentication.
+      # @option settings [String] :pass ("guest") Password to use for authentication.
+      # @option settings [String] :ssl (false) Should be use TLS (SSL) for connection?
+      # @option settings [String] :timeout (nil) Connection timeout.
+      # @option settings [String] :logging (false) Turns logging on or off.
+      # @option settings [String] :broker (nil) Broker name (use if you intend to use broker-specific features).
+      #
+      # @return [Hash] Merged configuration parameters.
       def self.configure(settings = nil)
         case settings
-        when Hash
+        when Hash then
           self.default.merge(settings)
-        when String
+        when String then
           settings = self.parse_amqp_url(settings)
           self.default.merge(settings)
-        when NilClass
+        when NilClass then
           self.default
         end
       end
