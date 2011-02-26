2159f562b931887651fa1a42a3a65043b5c73875
Michael S. Klishin <michael@novemberain.com>
2011-02-26 19:53:55 +0300

    With Adapter being a module, loggin shortcuts are not worth maintaining

--- spec/client/adapter_spec.rb
+++ spec/client/adapter_spec.rb
@@ -4,36 +4,28 @@ require "ostruct"
 require "spec_helper"
 require "amq/client/adapter"
 
-describe AMQ::Client::Adapter do
+class SampleAdapter
+  include AMQ::Client::Adapter
+end
+
+describe SampleAdapter do
   before(:all) do
     load "amq/client.rb"
   end
 
   describe ".settings" do
     it "should provide some default values" do
-      AMQ::Client::Adapter.settings.should_not be_nil
-      AMQ::Client::Adapter.settings[:host].should_not be_nil
-    end
-  end
-
-  describe "logging accessors" do
-    it "should be the same as AMQ::Client::Logging.logging" do
-      AMQ::Client::Adapter::logging.should == AMQ::Client::Logging.logging
-
-      AMQ::Client::Adapter::logging = ! AMQ::Client::Adapter::logging
-      AMQ::Client::Adapter::logging.should == AMQ::Client::Logging.logging
-
-      AMQ::Client::Logging.logging = ! AMQ::Client::Logging.logging
-      AMQ::Client::Adapter::logging.should == AMQ::Client::Logging.logging
+      described_class.settings.should_not be_nil
+      described_class.settings[:host].should_not be_nil
     end
   end
 
   describe ".logger" do
     it "should provide a default logger" do
-      AMQ::Client::Adapter.logger.should respond_to(:debug)
-      AMQ::Client::Adapter.logger.should respond_to(:info)
-      AMQ::Client::Adapter.logger.should respond_to(:error)
-      AMQ::Client::Adapter.logger.should respond_to(:fatal)
+      described_class.logger.should respond_to(:debug)
+      described_class.logger.should respond_to(:info)
+      described_class.logger.should respond_to(:error)
+      described_class.logger.should respond_to(:fatal)
     end
   end
 
@@ -41,16 +33,16 @@ describe AMQ::Client::Adapter do
     context "when new logger doesn't respond to all the necessary methods" do
       it "should raise an exception" do
         lambda {
-          AMQ::Client::Adapter.logger = Object.new
+          described_class.logger = Object.new
         }.should raise_error(AMQ::Client::Logging::IncompatibleLoggerError)
       end
 
       it "should pass if the object provides all the necessary methods" do
-        AMQ::Client::Adapter.logging = true
+        described_class.logging = true
 
         mock = OpenStruct.new(:debug => nil, :info => nil, :error => nil, :fatal => nil)
-        AMQ::Client::Adapter.logger = mock
-        AMQ::Client::Adapter.logger.should eql(mock)
+        described_class.logger = mock
+        described_class.logger.should eql(mock)
       end
     end
   end
