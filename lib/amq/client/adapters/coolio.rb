cae11962368c5546fd048882376c7d544a37c776
Mark Abramov <markizko@gmail.com>
2011-03-06 17:51:35 -0500

    Cool.io adapter for AMQ-Client. Basically, ripped off the EM-adapter.
    I don't see the reason why we shouldn't have the same API for different
    asynch adapters, so I ripped off examples as well, doing a slight tuning
    for cool.io.
    
    WIP.

--- lib/amq/client/adapters/coolio.rb
+++ lib/amq/client/adapters/coolio.rb
@@ -2,17 +2,6 @@
 
 # http://coolio.github.com
 
-# === Example ===
-# require "amq/client/adapters/coolio"
-#
-# AMQ.adapter(:coolio)
-# queue = Queue.new("tasks")
-# queue.consume do |headers, message|
-#   puts "Task scheduled: #{message}"
-# end
-#
-# cool.io.run
-
 require "cool.io"
 require "amq/client"
 
@@ -20,39 +9,128 @@ require "amq/client/io/string"
 
 module AMQ
   module Client
-    class CoolioClient < AMQ::Client::Adapter
-      self.sync = false
+    class Coolio
+      class Socket < ::Coolio::TCPSocket
+        attr_accessor :adapter
 
-      def establish_connection(settings)
-        cool.io.connect(settings[:host], settings[:port]) do
-          on_connect do
-            puts "Connected to #{remote_host}:#{remote_port}"
-            write "bounce this back to me"
-          end
+        def self.connect(adapter, host, port)
+          socket = super(host, port)
+          socket.adapter = adapter
+          socket
+        end
 
-          on_close do
-            puts "Disconnected from #{remote_host}:#{remote_port}"
-          end
+        def on_connect
+          #puts "On connect"
+          adapter.on_connect
+        end
 
-          on_read do |data|
-            puts "Got: #{data}"
-            close
-          end
+        def on_read(data)
+          # puts "Received data"
+          # puts_data(data)
+          adapter.on_read(data)
+        end
 
-          on_resolve_failed do
-            puts "Error: Couldn't resolve #{remote_host}"
-          end
+        def on_close
+          puts "Cool.io hit EOF"
+        end
 
-          on_connect_failed do
-            puts "Error: Connection refused to #{remote_host}:#{remote_port}"
-          end
+        protected
+        def puts_data(data)
+          puts "    As string:     #{data.inspect}"
+          puts "    As byte array: #{data.bytes.to_a.inspect}"
         end
       end
 
-      def close_connection
+      # Behaviors
+      include AMQ::Client::Adapter
+
+      self.sync = false
+
+      # API
+      attr_accessor :socket
+      attr_accessor :callbacks
+
+      class <<self
+        def connect(settings, &block)
+          settings        = self.settings.merge(settings)
+          host, port      = settings[:host], settings[:port]
+          instance        = new
+          socket          = Socket.connect(instance, settings[:host], settings[:port])
+          socket.attach Cool.io::Loop.default
+          instance.socket = socket
+          instance.on_connection(&block)
+          instance
+        end
+
+      end
+
+      def initialize
+        @callbacks = {}
+        super
+      end
+
+      # sets a callback for connection
+      def on_connection(&block)
+        @callbacks[:connect] = block
+      end
+
+      # sets a callback for disconnection
+      def on_disconnection(&block)
+        @callbacks[:disconnect] = block
+      end
+
+      # triggered when socket is connected but before handshake is done
+      def on_connect
+        post_init
+      end
+
+
+      # triggered after socket is disconnected
+      def on_disconnect
+      end
+
+      def post_init
+        reset
+        handshake
       end
 
       def send_raw(data)
+        puts "Sending raw: #{[data, data.bytes.to_a].inspect}"
+        socket.write data
+      end
+
+      # The story about the buffering is kinda similar to EventMachine,
+      # you keep receiving more than one frame in a single packet.
+      def on_read(chunk)
+        @chunk_buffer << chunk
+        while frame = get_next_frame
+          receive_frame(AMQ::Client::StringAdapter::Frame.decode(frame))
+        end
+      end
+
+      # called by AMQ::Client::Connection after we receive connection.open-ok.
+      def connection_successful
+        @callbacks[:connect].call(self) if @callbacks[:connect]
+      end
+
+      # called by AMQ::Client::Connection after we receive connection.close-ok.
+      def disconnection_successful
+        @callbacks[:disconnect].call(self) if @callbacks[:disconnect]
+      end
+
+      protected
+      def reset
+        @chunk_buffer = ""
+      end
+
+      def encode_credentials(username, password)
+        "\0guest\0guest"
+      end # encode_credentials(username, password)
+
+      def get_next_frame
+        if pos = @chunk_buffer.index(AMQ::Protocol::Frame::FINAL_OCTET)
+          @chunk_buffer.slice!(0, pos + 1)
+        end
       end
     end
   end
