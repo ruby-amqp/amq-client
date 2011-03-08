28c3ab9d5a3377056958e51b2f4768a672ea4c18
Theo <theo@iconara.net>
2011-03-08 07:02:13 +0100

    Removed the need to check Ruby version in io/io.rb
    str[x] doesn't have the same behaviour in 1.8 and 1.9, but str[x, 1] does.

--- lib/amq/client/io/io.rb
+++ lib/amq/client/io/io.rb
@@ -10,13 +10,7 @@ module AMQ
           header = io.read(7)
           type, channel, size = self.decode_header(header)
           data = io.read(size + 1)
-          if RUBY_VERSION < "1.9"
-            # The m flag's important, the data often contains "\n":
-            data.match(/^(.+)(.)$/m)
-            payload, frame_end = $1, $2
-          else
-            payload, frame_end = data[0..-2], data[-1]
-          end
+          payload, frame_end = data[0..-2], data[-1, 1]
           # TODO: this will hang if the size is bigger than expected or it'll leave there some chars -> make it more error-proof:
           # BTW: socket#eof?
           raise NoFinalOctetError.new if frame_end != AMQ::Protocol::Frame::FINAL_OCTET
