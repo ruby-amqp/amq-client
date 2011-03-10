0ecd1e1516cc984382bfedc4e914c4c191ce681a
Michael S. Klishin <michael@novemberain.com>
2011-03-10 21:33:22 +0300

    Keep existing spec examples under spec/unit

---
+++ spec/unit/client/io/io_spec.rb
@@ -0,0 +1,57 @@
+# encoding: utf-8
+
+require "spec_helper"
+require "stringio"
+
+# We need to require AMQ-Protocol manually.
+# In the library this is required in the file
+# amq/client.rb, but this is a unit test and
+# we don't want to mess around with unecessary
+# dependencies.
+require "amq/protocol/client"
+require "amq/protocol/frame"
+
+# We have to use Kernel#load so extensions to the
+# Logging module from client.rb will be overridden.
+load "amq/client/io/io.rb"
+
+describe AMQ::Client::IOAdapter do
+  subject do
+    AMQ::Client::IOAdapter::Frame
+  end
+
+  # Created by:
+  # frame = AMQ::Protocol::Queue::Declare.encode(1, "tasks", false, false, false, false, {})
+  # frame.encode
+  # frame.payload
+  before do
+    data = ["\x01\x00\x00\x00\x00\x00\b"]
+    data << "\x00\n\x00(\x01/\x00\x00"
+    data << "\xCE"
+    @io = StringIO.new(data.join)
+
+    subject.stub(:decode_header).with(data.first).and_return([1, 0, data[1].bytesize])
+  end
+
+  it "should be able to decode frame type" do
+    subject.decode(@io).should be_kind_of(AMQ::Protocol::MethodFrame)
+  end
+
+  it "should be able to decode channel" do
+    subject.decode(@io).channel.should eql(0)
+  end
+
+  it "should be able to decode payload" do
+    subject.decode(@io).payload.should eql("\x00\n\x00(\x01/\x00\x00")
+  end
+
+  it "should raise an error if the frame length is miscalculated" do
+    pending "How to do that with socket?"
+  end
+
+  it "should raise an error if the frame doesn't end with FINAL_OCTET" do
+    data = @io.read[0..-2] + "too long" + "\xCE"
+    io   = StringIO.new(data)
+    lambda { subject.decode(io) }.should raise_error(AMQ::Client::NoFinalOctetError)
+  end
+end
