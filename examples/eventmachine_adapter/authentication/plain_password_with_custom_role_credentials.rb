144ef1aad25ea9ff36bb603276a59fdf246613f0
Michael S. Klishin <michael@novemberain.com>
2011-03-10 13:01:59 +0300

    Several authentication examples

---
+++ examples/eventmachine_adapter/authentication/plain_password_with_custom_role_credentials.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+__dir = File.join(File.dirname(File.expand_path(__FILE__)), "..")
+require File.join(__dir, "example_helper")
+
+EM.run do
+  AMQ::Client::EventMachineClient.connect(:port     => 5672,
+                                          :vhost    => "/amq_client_testbed",
+                                          :user     => "amq_client_gem",
+                                          :password => "amq_client_gem_password") do |client|
+    puts "Connected, authenticated"
+
+
+    show_stopper = Proc.new {
+      client.disconnect do
+        puts
+        puts "AMQP connection is now properly closed"
+        EM.stop
+      end
+    }
+
+    Signal.trap "INT",  show_stopper
+    Signal.trap "TERM", show_stopper
+  end
+end
---
+++ examples/eventmachine_adapter/authentication/plain_password_with_default_role_credentials.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+__dir = File.join(File.dirname(File.expand_path(__FILE__)), "..")
+require File.join(__dir, "example_helper")
+
+EM.run do
+  AMQ::Client::EventMachineClient.connect(:port => 5672, :vhost => "/amq_client_testbed", :user => "guest", :password => "guest") do |client|
+    puts "Connected, authenticated"
+
+
+    show_stopper = Proc.new {
+      client.disconnect do
+        puts
+        puts "AMQP connection is now properly closed"
+        EM.stop
+      end
+    }
+
+    Signal.trap "INT",  show_stopper
+    Signal.trap "TERM", show_stopper
+  end
+end
