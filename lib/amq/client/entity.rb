9ac77b59269486a118ebac934a07bdf0cd2af6a7
Michael S. Klishin <michael@novemberain.com>
2011-03-15 02:05:09 +0300

    Only execute callbacks if they are not nil

--- lib/amq/client/entity.rb
+++ lib/amq/client/entity.rb
@@ -45,6 +45,11 @@ module AMQ
 
       def exec_callback_once(name, *args, &block)
         callback = self.callbacks.delete(name)
+        callback.call(self, *args, &block) if callback
+      end
+
+      def exec_callback_once(name, *args, &block)
+        callback = self.callbacks.delete(name)
         callback.call(*args, &block) if callback
       end
 
