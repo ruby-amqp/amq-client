9b036fd283b95ba56e97b6227eaf813d14983dbe
Mark Abramov <markizko@gmail.com>
2011-03-13 21:10:10 -0500

    Added a failing spec

--- spec/integration/coolio/basic_consume_spec.rb
+++ spec/integration/coolio/basic_consume_spec.rb
@@ -35,4 +35,36 @@ describe AMQ::Client::Coolio do
     end
 
   end
+
+  # @todo: investigate, why this fails and the other does not.
+  context "sending 1000 messages" do
+    let(:messages) { (0..999).map {|i| "Message #{i}" } }
+
+    it "should receive all the messages" do
+      @received_messages = []
+      coolio do
+        amqp_connect do |client|
+          channel = AMQ::Client::Channel.new(client, 1)
+          channel.open do end
+          queue = AMQ::Client::Queue.new(client, channel)
+          queue.declare(false, false, false, true)
+          queue.bind("amq.fanout")
+          queue.consume(true) do |_, consumer_tag|
+            queue.on_delivery do |_, header, payload, consumer_tag, delivery_tag, redelivered, exchange, routing_key|
+              @received_messages << payload
+              done if @received_messages.size == messages.size
+            end
+
+            exchange = AMQ::Client::Exchange.new(client, channel, "amq.fanout", :fanout)
+            messages.each do |message|
+              exchange.publish(message)
+            end
+          end
+        end
+      end
+
+      @received_messages.should =~ messages
+    end
+
+  end
 end
