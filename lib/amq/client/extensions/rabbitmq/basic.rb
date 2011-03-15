9c5aa4e9635179ddef1ec814da709257faf35882
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-03-15 16:22:55 +0000

    Support for Basic.Nack in the RabbitMQ extension.
      * Extensions::RabbitMQ::Channel#reject(delivery_tag, requeue = true, multi = false)

--- lib/amq/client/extensions/rabbitmq/basic.rb
+++ lib/amq/client/extensions/rabbitmq/basic.rb
@@ -1,3 +1,23 @@
 # encoding: utf-8
 
+require "amq/client/amqp/channel"
+
 # Basic.Nack
+module AMQ
+  module Client
+    module Extensions
+      module RabbitMQ
+        class Channel < ::AMQ::Client::Channel
+          # http://www.rabbitmq.com/amqp-0-9-1-quickref.html#basic.nack
+          def reject(delivery_tag, requeue = true, multi = false)
+            if multi
+              @client.send(Protocol::Basic::Nack.encode(self.id, delivery_tag, multi, requeue))
+            else
+              super(delivery_tag, requeue)
+            end
+          end
+        end
+      end
+    end
+  end
+end
