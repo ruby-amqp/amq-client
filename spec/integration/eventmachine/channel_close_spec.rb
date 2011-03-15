bd0bac5b826a02c27dcee305bf22390af67744f6
Mark Abramov <markizko@gmail.com>
2011-03-15 12:28:27 -0500

    Channel.Close specs

---
+++ spec/integration/eventmachine/channel_close_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'integration/eventmachine/spec_helper'
+
+describe AMQ::Client::EventMachineClient, "Channel.Close" do
+  include EventedSpec::SpecHelper
+  default_timeout 1
+
+  it "should close the channel" do
+    @events = []
+    em_amqp_connect do |client|
+      @events << :connect
+      channel = AMQ::Client::Channel.new(client, 1)
+      channel.open do
+        @events << :open
+        channel.close do
+          @events << :close
+          client.disconnect do
+            @events << :disconnect
+            done
+          end
+        end
+      end
+    end
+    @events.should == [:connect, :open, :close, :disconnect]
+  end
+end
