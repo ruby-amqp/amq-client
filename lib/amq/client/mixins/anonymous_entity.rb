392e2d3a2506f1e59a7cd9cad27f55ff7ef9cb82
Michael S. Klishin <michael@novemberain.com>
2011-03-16 22:27:32 +0300

    Undo ROFLSCALE, don't use instance_variable_set insanity, don't treat all objects as queues

--- lib/amq/client/mixins/anonymous_entity.rb
+++ lib/amq/client/mixins/anonymous_entity.rb
@@ -11,12 +11,10 @@ module AMQ
       end
 
       def dup
-        if @name.eql?(AMQ::Protocol::EMPTY_STRING)
+        if @name.empty?
           raise RuntimeError.new("You can't clone anonymous queue until it receives back the name in Queue.Declare-Ok response. Move the code with #dup to the callback for the #declare method.") # TODO: that's not true in all cases, imagine the user didn't call #declare yet.
         end
-        instance = self.dup
-        instance.instance_variable_set(:@consumer_tag, nil)
-        instance
+        super
       end
     end # AnonymousEntityMixin
   end # Client
