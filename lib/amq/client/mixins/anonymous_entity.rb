864b3fc120fd4085fe3f77d3aed00216ccf362a1
Michael S. Klishin <michael@novemberain.com>
2011-03-16 22:10:18 +0300

    With this groundbreaking innovation, we are about to hit ROFLSCALE

--- lib/amq/client/mixins/anonymous_entity.rb
+++ lib/amq/client/mixins/anonymous_entity.rb
@@ -11,13 +11,13 @@ module AMQ
       end
 
       def dup
-        if @name.eql?("")
+        if @name.eql?(AMQ::Protocol::EMPTY_STRING)
           raise RuntimeError.new("You can't clone anonymous queue until it receives back the name in Queue.Declare-Ok response. Move the code with #dup to the callback for the #declare method.") # TODO: that's not true in all cases, imagine the user didn't call #declare yet.
         end
         instance = self.dup
         instance.instance_variable_set(:@consumer_tag, nil)
         instance
       end
-    end
-  end
-end
+    end # AnonymousEntityMixin
+  end # Client
+end # AMQ
