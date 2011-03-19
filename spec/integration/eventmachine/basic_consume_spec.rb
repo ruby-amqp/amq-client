f023eb8a53cc22c0b0b4df950dc98c68764d4c7c
Michael S. Klishin <michael@novemberain.com>
2011-03-19 16:15:31 +0300

    Use just one context

--- spec/integration/eventmachine/basic_consume_spec.rb
+++ spec/integration/eventmachine/basic_consume_spec.rb
@@ -5,35 +5,6 @@ describe AMQ::Client::EventMachineClient, "Basic.Consume" do
   include EventedSpec::SpecHelper
   default_timeout 2
 
-  context "sending 100 messages" do
-    let(:messages) { (0..99).map {|i| "Message #{i}" } }
-
-    it "should receive all the messages" do
-      @received_messages = []
-      em_amqp_connect do |client|
-        channel = AMQ::Client::Channel.new(client, 1)
-        channel.open do end
-        queue = AMQ::Client::Queue.new(client, channel)
-        queue.declare(false, false, false, true)
-        queue.bind("amq.fanout")
-        queue.consume(true) do |_, consumer_tag|
-          queue.on_delivery do |header, payload, consumer_tag, delivery_tag, redelivered, exchange, routing_key|
-            @received_messages << payload
-            done if @received_messages.size == messages.size
-          end
-
-          exchange = AMQ::Client::Exchange.new(client, channel, "amq.fanout", :fanout)
-          messages.each do |message|
-            exchange.publish(message)
-          end
-        end
-      end
-
-      @received_messages.should =~ messages
-    end
-
-  end
-
   context "sending 1000 messages" do
     let(:messages) { (0..999).map {|i| "Message #{i}" } }
 
@@ -60,6 +31,5 @@ describe AMQ::Client::EventMachineClient, "Basic.Consume" do
 
       @received_messages.should =~ messages
     end
-
   end
 end
