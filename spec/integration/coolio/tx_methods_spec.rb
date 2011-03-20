754da46b7800f5858cb55296c368ee2c4d60e1d3
Mark Abramov <markizko@gmail.com>
2011-03-20 14:43:07 -0500

    Tx.* methods spec

---
+++ spec/integration/coolio/tx_methods_spec.rb
@@ -0,0 +1,63 @@
+# encoding: utf-8
+require 'spec_helper'
+require 'integration/coolio/spec_helper'
+
+describe AMQ::Client::Coolio, "Tx.*" do
+  include EventedSpec::SpecHelper
+  default_timeout 2
+
+  context "Tx.Commit" do
+    let(:message) { "Hello, world!" }
+    it "should confirm transaction completeness" do
+      coolio_amqp_connect do |client|
+        channel = AMQ::Client::Channel.new(client, 1)
+        channel.open do
+          exchange = AMQ::Client::Exchange.new(client, channel, "amq.fanout", :fanout)
+          queue = AMQ::Client::Queue.new(client, channel)
+
+          queue.declare(false, false, false, true) do
+            queue.bind(exchange)
+          end
+
+          channel.tx_select do
+            exchange.publish(message)
+            channel.tx_commit do
+              queue.get(true) do |header, payload, delivery_tag, redelivered, exchange, routing_key, message_count|
+                payload.should == message
+                done
+              end
+            end
+          end
+       end
+      end
+    end
+  end
+
+
+  context "Tx.Rollback" do
+    let(:message) { "Hello, world!" }
+    it "should cancel all the changes done during transaction" do
+      coolio_amqp_connect do |client|
+        channel = AMQ::Client::Channel.new(client, 1)
+        channel.open do
+          exchange = AMQ::Client::Exchange.new(client, channel, "amq.fanout", :fanout)
+          queue = AMQ::Client::Queue.new(client, channel)
+
+          queue.declare(false, false, false, true) do
+            queue.bind(exchange)
+          end
+
+          channel.tx_select do
+            exchange.publish(message)
+            channel.tx_rollback do
+              queue.get(true) do |header, payload, delivery_tag, redelivered, exchange, routing_key, message_count|
+                payload.should == nil
+                done
+              end
+            end
+          end
+       end
+      end
+    end
+  end
+end
