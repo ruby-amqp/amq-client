02de31a90e5e2aa0fc2ba8ad0c1e0cd8e582a858
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-04-18 14:47:11 +0100

    Unified API for connect via AMQ::Client.connect.
      * AMQ::Client.adapters
      * AMQ::Client.connect(settings), i. e.:
        AMQ::Client.connect(adapter: :socket, host: "localhost")
      * AMQ::Client.load_adapter(adapter)

--- lib/amq/client.rb
+++ lib/amq/client.rb
@@ -16,3 +16,58 @@ rescue LoadError => exception
     raise exception
   end
 end
+
+module AMQ
+  module Client
+    # List all the available as a hash of {adapter_name: metadata},
+    # where metadata are hash with :path and :const_name keys.
+    #
+    # @example
+    #   AMQ::Client.adapters[:socket]
+    #   # => {path: full_path, const_name: "SocketClient"}}
+    # @return [Hash]
+    # @api public
+    def self.adapters
+      @adapters ||= begin
+        root = File.expand_path("../client/adapters", __FILE__)
+        Dir.glob("#{root}/*.rb").inject(Hash.new) do |buffer, path|
+          path.match(/([^\/]+)\.rb$/) do |match|
+            name = match[1]
+            const_base = name.to_s.gsub(/(^|_)(.)/) { $2.upcase! }
+            meta = {:path => path, :const_name => "#{const_base}Client"}
+            buffer.merge!(name.to_sym => meta)
+          end
+        end
+      end
+    end
+
+    # Establishes connection to AMQ broker using given adapter
+    # (defaults to the socket adapter) and returns it. The new
+    # connection object is yielded to the block if it is given.
+    #
+    # @example
+    #   AMQ::Client.connect(adapter: "socket") do |client|
+    #     # Use the client.
+    #   end
+    # @param [Hash] Connection parameters, including :adapter to use.
+    # @api public
+    def self.connect(settings = nil, &block)
+      adapter  = (settings && settings.delete(:adapter)) || :socket
+      adapter  = load_adapter(adapter)
+      settings = Settings.configure(settings)
+      adapter.connect(settings, &block)
+    end
+
+    # Loads adapter from amq/client/adapters.
+    #
+    # @raise [InvalidAdapterNameError] When loading attempt failed (LoadError was raised).
+    def self.load_adapter(adapter)
+      meta = self.adapters[adapter.to_sym]
+
+      require meta[:path]
+      const_get(meta[:const_name])
+    rescue LoadError
+      raise InvalidAdapterNameError.new(adapter)
+    end
+  end
+end
--- spec/unit/client_spec.rb
+++ spec/unit/client_spec.rb
@@ -5,7 +5,72 @@ require "spec_helper"
 require "amq/client"
 
 describe AMQ::Client do
+  ADAPTERS = {
+    :event_machine => "EventMachineClient",
+    :coolio        => "CoolioClient",
+    :socket        => "SocketClient"
+  }
+
   it "should have VERSION" do
     AMQ::Client::const_defined?(:VERSION).should be_true
+  end
+
+  ADAPTERS.each do |adapter_name, adapter_const_name|
+    describe ".adapters" do
+      before(:all) do
+        @meta = AMQ::Client.adapters[adapter_name]
+      end
+
+      it "should provide info about path to the adapter" do
+        lambda { require @meta[:path] }.should_not raise_error
+      end
+
+      it "should provide info about const_name" do
+        @meta[:const_name].should eql(adapter_const_name)
+      end
+    end
+  end
+
+  describe ".connect(settings = nil, &block)" do
+    include EventedSpec::SpecHelper
+    default_timeout 1
+
+    context "with specified adapter" do
+      it "should connect using socket adapter" do
+        pending "Socket adapter is currently broken" do
+          AMQ::Client.connect(:adapter => :socket) do |client|
+            client.class.name.should eql("AMQ::Client::SocketClient")
+            done
+          end
+        end
+      end
+
+      it "should connect using event_machine adapter" do
+        em do
+          AMQ::Client.connect(:adapter => :event_machine) do |client|
+            client.class.name.should eql("AMQ::Client::EventMachineClient")
+            done
+          end
+        end
+      end
+
+      it "should connect using coolio adapter" do
+        AMQ::Client.connect(:adapter => :coolio) do |client|
+          client.class.name.should eql("AMQ::Client::CoolioClient")
+          done
+        end
+      end
+    end
+
+    context "without any specified adapter" do
+      it "should default to the socket adapter" do
+        pending "Socket adapter is currently broken" do
+          AMQ::Client.connect do |client|
+            client.class.name.should eql("AMQ::Client::SocketClient")
+            done
+          end
+        end
+      end
+    end
   end
 end
