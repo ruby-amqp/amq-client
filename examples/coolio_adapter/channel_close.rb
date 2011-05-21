1ef576135aeae22d2e4220c3fc54fd4cc9786585
Michael S. Klishin <michael@novemberain.com>
2011-05-21 13:48:45 +0400

    Update this example

--- examples/coolio_adapter/channel_close.rb
+++ examples/coolio_adapter/channel_close.rb
@@ -4,17 +4,17 @@
 __dir = File.dirname(File.expand_path(__FILE__))
 require File.join(__dir, "example_helper")
 
-amq_client_example "Open and then close AMQ channel" do |client|
-  puts "AMQP connection is open: #{client.connection.server_properties.inspect}"
+amq_client_example "Open and then close AMQ channel" do |connection|
+  puts "AMQP connection is open: #{connection.server_properties.inspect}"
 
-  channel = AMQ::Client::Channel.new(client, 1)
+  channel = AMQ::Client::Channel.new(connection, 1)
   channel.open do
     puts "Channel #{channel.id} is now open!"
     puts "Lets close it."
     channel.close do
       puts "Closed channel ##{channel.id}"
       puts
-      client.disconnect do
+      connection.disconnect do
         puts
         puts "AMQP connection is now properly closed"
         Coolio::Loop.default.stop
