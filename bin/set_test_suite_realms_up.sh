ba0a18777ad7af8e03a099506d36148c2096687b
Michael S. Klishin <michael@novemberain.com>
2011-05-24 20:25:05 +0400

    Use vhost names in line with ruby-amqp/amqp#77 (367ed50dec)

--- bin/set_test_suite_realms_up.sh
+++ bin/set_test_suite_realms_up.sh
@@ -7,18 +7,18 @@ rabbitmqctl add_user guest guest
 rabbitmqctl set_permissions -p / guest ".*" ".*" ".*"
 
 
-# guest:guest has full access to /amq_client_testbed
+# guest:guest has full access to amq_client_testbed
 # amq_client_gem:amq_client_gem has full access to /amq_client_testbed
 
-rabbitmqctl delete_vhost "/amq_client_testbed"
-rabbitmqctl add_vhost "/amq_client_testbed"
+rabbitmqctl delete_vhost "amq_client_testbed"
+rabbitmqctl add_vhost "amq_client_testbed"
 rabbitmqctl delete_user amq_client_gem
 rabbitmqctl add_user amq_client_gem amq_client_gem_password
-rabbitmqctl set_permissions -p /amq_client_testbed guest ".*" ".*" ".*"
-rabbitmqctl set_permissions -p /amq_client_testbed amq_client_gem ".*" ".*" ".*"
+rabbitmqctl set_permissions -p amq_client_testbed guest ".*" ".*" ".*"
+rabbitmqctl set_permissions -p amq_client_testbed amq_client_gem ".*" ".*" ".*"
 
 
-# amqp_gem_reader:reader_password has read access to /amq_client_testbed
+# amqp_gem_reader:reader_password has read access to amq_client_testbed
 
 rabbitmqctl add_user amq_client_gem_reader reader_password
-rabbitmqctl set_permissions -p /amq_client_testbed amq_client_gem_reader "^---$" "^---$" ".*"
+rabbitmqctl set_permissions -p amq_client_testbed amq_client_gem_reader "^---$" "^---$" ".*"
