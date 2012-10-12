81b8a33372b877956030519011a3314c2a2d27a1
Michael S. Klishin <michael@novemberain.com>
2012-10-12 11:03:00 +0400

    Add spec example of consumer cancellation notification on an arbitrary consumer

--- spec/integration/eventmachine/consumer_cancellation_notification_spec.rb
+++ spec/integration/eventmachine/consumer_cancellation_notification_spec.rb
@@ -37,6 +37,40 @@ describe AMQ::Client::EventMachineClient, "basic.cancel notification" do
         }
       end
     end
+  end # it
 
-  end # it "should stop receiving messages after receiving cancel-ok"
+  it "works for other consumers" do
+    @cancellation_notifications = []
+
+    em_amqp_connect do |client|
+      channel = AMQ::Client::Channel.new(client, 1)
+      channel.open do
+        queue = AMQ::Client::Queue.new(client, channel).declare(false, false, false, true)
+        queue.bind("amq.fanout")
+        exchange = AMQ::Client::Exchange.new(client, channel, "amq.fanout", :fanout)
+
+        consumer1 = AMQ::Client::Async::Consumer.new(channel, queue, "#{queue.name}-consumer-#{Time.now}-#{rand}")
+        consumer2 = AMQ::Client::Async::Consumer.new(channel, queue, "#{queue.name}-consumer-#{Time.now}-#{rand}")
+        consumer1.consume do |_|
+          messages.each do |message|
+            exchange.publish(message)
+          end
+        end
+        consumer2.consume
+
+        consumer1.on_cancel do |basic_cancel|
+          @cancellation_notifications << 1
+        end
+        consumer2.on_cancel do |basic_cancel|
+          @cancellation_notifications << 2
+        end
+
+        delayed(1.0) { queue.delete }
+
+        done(2.5) {
+          @cancellation_notifications.sort.should == [1, 2]
+        }
+      end
+    end
+  end # it
 end # describe AMQ::Client::EventMachineClient, "Basic.Consume"
